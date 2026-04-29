807c2f8feaa30e56f3bd1c70972482728cfe4323
ferdiansurya2022-ui <ferdiansurya.2022@student.uny.ac.id>
2026-04-29 15:34:55 +0700

    Update config.ts

--- artifacts/learning-path/src/lib/config.ts
+++ artifacts/learning-path/src/lib/config.ts
@@ -1,4 +1,4 @@
 export const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyXhdSc4yNCp-c8ouC4LESlgogzfCs-X0HtHpfK713mGy673DLDYPUiQFkL3cjNhya9TA/exec";
 export const SPREADSHEET_URL = "https://docs.google.com/spreadsheets/d/1W-2YOhp3zFks_UD5P56rj0WycZ_6Gn1Fs105Cg3ePIA/edit?pli=1&gid=194168401#gid=194168401";
-export const API_BASE = "https://api.sistemai.my.id";
+export const API_BASE = "https://small-otters-drum.loca.lt";
 export const PASSING_SCORE = 75;

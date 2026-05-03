39d205789fcdb0ad4349a44efb6105be4f379a05
ferdiansurya2022-ui <ferdiansurya.2022@student.uny.ac.id>
2026-05-03 10:24:23 +0700

    Update config.ts

--- artifacts/learning-path/src/lib/config.ts
+++ artifacts/learning-path/src/lib/config.ts
@@ -1,4 +1,4 @@
-export const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyXhdSc4yNCp-c8ouC4LESlgogzfCs-X0HtHpfK713mGy673DLDYPUiQFkL3cjNhya9TA/exec";
+export const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyUZEYxsiJlu4SUBPqyHxZkTRnMbwDwv3xldOfVev8hoUEbOqAD7mpD49lZOOtwsqc_IQ/exec";
 export const SPREADSHEET_URL = "https://docs.google.com/spreadsheets/d/1W-2YOhp3zFks_UD5P56rj0WycZ_6Gn1Fs105Cg3ePIA/edit?pli=1&gid=194168401#gid=194168401";
 export const API_BASE = "https://silent-turkeys-watch.loca.lt";
 export const PASSING_SCORE = 75;
